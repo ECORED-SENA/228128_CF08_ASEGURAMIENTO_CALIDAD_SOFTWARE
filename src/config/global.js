4361923ff125966462ef9e5811676f2bfb572b50
Luis Pérez <ljpm9401@gmail.com>
2024-04-10 13:59:28 -0500

    AJustes

--- src/config/global.js
+++ src/config/global.js
@@ -147,7 +147,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/prueba.pdf',
+        download: 'downloads/228128_CF08_DU.zip',
       },
       {
         icono: 'fas fa-download',
